a57540e8a7f330694958856bf5d81b2fad6f85d3
Brody <brodyuehara@gmail.com>
2020-06-29 11:22:14 -1000

    Neater code in OOP form

--- AccountPositions.py
+++ AccountPositions.py
@@ -3,27 +3,45 @@ import pandas as pd
 import time
 # util.startLoop()  # uncomment this line when in a notebook
 
-ib = IB()
-ib.connect('127.0.0.1', 7496, clientId=1)
+class TWSAPI:
 
-accounts = ib.managedAccounts()
+    # Function to pull position data from managed accounts
+    def positionsData(self):
+        
+        accounts = ib.managedAccounts()
+        final_df = pd.DataFrame()
 
-final_df = pd.DataFrame()
+        for account in accounts:
+            stocks = []
+            quantity = []
 
-for account in accounts:
-    stocks = []
-    quantity = []
+            positions = ib.positions(account)
 
-    positions = ib.positions(account)
+            for i in range(len(positions)):
+                stocks.append(positions[i].contract.localSymbol)
+                quantity.append(positions[i].position)
 
-    for i in range(len(positions)):
-        stocks.append(positions[i].contract.localSymbol)
-        quantity.append(positions[i].position)
+            df = pd.DataFrame(stocks, columns=[account])
+            df['Quantity'] = quantity
+            final_df = pd.concat([final_df, df], axis=1)
 
-    df = pd.DataFrame(stocks, columns=[account])
-    df['Quantity'] = quantity
-    final_df = pd.concat([final_df, df], axis=1)
+        tm = time.strftime('%d %b %y')
+        final_df.to_csv('positionsData {}.csv'.format(tm))
 
-print(final_df.head())
-tm = time.strftime('%d %b %y')
-final_df.to_csv('positionsData {}.csv'.format(tm))
+# Main
+if __name__ == "__main__":
+    # Try to connect to TWS API, throws error if 
+    while True:
+        try:
+            ib = IB()
+            ib.connect('127.0.0.1', 7496, clientId=1)
+            break
+        except ValueError:
+            print("Could not connect to TWS: Check if TWS is runnning & correct port gate...")
+
+    print("Connection established...")
+    print("Pulling positions data for managed accounts...")
+    
+    apicall = TWSAPI()
+    apicall.positionsData()
+
